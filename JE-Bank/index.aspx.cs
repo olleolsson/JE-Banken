9eee5ded72f8f1000b5ebe76e9726bd8ad92056d
Nathalie <naol1501>
2016-10-25 12:03:01 +0200

    Fixat så att man kan hämta alla fråor från xml och skriva ut dom

--- JE-Bank/index.aspx.cs
+++ JE-Bank/index.aspx.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
+using System.Xml;
 
 namespace JE_Bank
 {
@@ -11,6 +13,40 @@ namespace JE_Bank
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+           AppendProv(xmlToList());
+        }
+
+        public void AppendProv(List<Prov> frågor) 
+        {
+            foreach (Prov f in frågor) 
+            {
+                HtmlGenericControl div = new HtmlGenericControl("div");
+                div.InnerText = f.Fråga;
+                allafrågor.Controls.Add(div);
+            
+            }
+        
+        }
+
+
+        public List<Prov> xmlToList() 
+        {
+        List<Prov> Lillatestet = new List<Prov>();
+
+        string path = Server.MapPath("lillaTestet.xml");
+        XmlDocument doc = new XmlDocument();
+        doc.Load(path);
+
+        XmlNodeList allafrågor = doc.SelectNodes("/quiz/Frågor/Produkter/fråga");
+
+        foreach (XmlNode node in allafrågor)
+        {
+            Prov f = new Prov();
+            f.Fråga = node["Frågan"].InnerText;
+            Lillatestet.Add(f);
+        }
+
+        return Lillatestet;       
 
         }
     }
