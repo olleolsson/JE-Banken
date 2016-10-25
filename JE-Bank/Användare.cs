78d0572087915dabe767090633a5fd1efc2188bb
Daniel Edvinsson <daed1500@student.miun.se>
2016-10-25 14:11:09 +0200

    Här kommer classen Användare!

---
+++ JE-Bank/Användare.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JE_Bank
+{
+    public class Användare
+    {
+        public string Anvädarnamn { get; set; }
+        public string Användartyp { get; set; }
+        public bool Certifierad { get; set; }
+    }
+}
