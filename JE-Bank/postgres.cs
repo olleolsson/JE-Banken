b110433630a11a13d57585601f64f238c3a393c6
Daniel Edvinsson <daed1500@student.miun.se>
2016-10-25 12:28:25 +0200

    Skapat koppling mot databas, gjort testfråga.

--- JE-Bank/postgres.cs
+++ JE-Bank/postgres.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Npgsql;
+using System.Configuration;
 
 namespace JE_Bank
 {
     public class postgres
     {
+
+        private NpgsqlConnection conn;
+        private NpgsqlCommand cmd;
+        private NpgsqlDataReader dr;
+
+        // Skapar koppling mot databas
+        static void PgOpen()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=webblabb.miun.se;Port=5432;User Id=interaktiva_g26;Password=bankapp;Database=interaktiva_g26;SslMode=Require;");
+            conn.Open();
+        }
+
+        static void PgClose()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=webblabb.miun.se;Port=5432;User Id=interaktiva_g26;Password=bankapp;Database=interaktiva_g26;SslMode=Require;");
+            conn.Close();
+        }
+
+        public void TestSqlFråga()
+        {
+            PgOpen();
+
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM användare");
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+
+
+            PgClose();
+        }
+
+
+
     }
 }
