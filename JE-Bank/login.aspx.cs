b733a0c49394f3dfd2afd020ea71260193de63c9
victor <olleolsson100@gmail.com>
2016-10-25 11:52:49 +0200

    Förberett inloggning med hårdkodning tills sqlfråga är redo

--- JE-Bank/login.aspx.cs
+++ JE-Bank/login.aspx.cs
@@ -9,6 +9,21 @@ namespace JE_Bank
 {
     public partial class logIn : System.Web.UI.Page
     {
+        public string a1 = "1";
+        public string p1 = "1";
+
+        public string a2 = "2";
+        public string p2 = "2";
+
+        public string a3 = "3";
+        public string p3 = "3";
+
+        public string a4 = "4";
+        public string p4 = "4";
+
+        public string aAdmin = "admin";
+        public string pAdmin = "admin";
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,7 +31,32 @@ namespace JE_Bank
 
         public void logIn
         {
-            btnLogIn1
+
+        }
+
+        protected void btnAdmin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogIn1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogIn2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogIn3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogIn4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
