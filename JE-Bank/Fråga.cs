25c7e70edade301c29c032c7caf8f66c0a33847e
Victor Olsson <olleolsson100@gmail.com>
2016-10-26 11:27:43 +0200

    Fixat class

---
+++ JE-Bank/Fråga.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JE_Bank
+{
+    public class Fråga
+    {
+        public string fråga { get; set; }
+        public List<Svar> svarsalternativ { get; set; }
+    }
+}
