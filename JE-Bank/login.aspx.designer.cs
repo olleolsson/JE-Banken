5fb35c4cc670688683e9dd462333641c7928202d
Victor Olsson <olleolsson100@gmail.com>
2016-10-27 10:57:12 +0200

    Fixat s¨å man får rätt test när man loggar in

--- JE-Bank/login.aspx.designer.cs
+++ JE-Bank/login.aspx.designer.cs
@@ -56,5 +56,14 @@ namespace JE_Bank {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnAdmin;
+        
+        /// <summary>
+        /// allafrågor control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl allafrågor;
     }
 }
