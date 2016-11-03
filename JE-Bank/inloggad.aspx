0f2926fa7f7075106cb2570c09eb4a7ef1d7ee27
victor <olleolsosn100@gmailc.om>
2016-11-03 09:54:58 +0100

    Lagt till sida för att starta test ifrån och se historik

---
+++ JE-Bank/inloggad.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="inloggad.aspx.cs" Inherits="JE_Bank.inloggad" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+        <asp:Button ID="btnStartaTest" runat="server" Text="Starta Test" OnClick="btnStartaTest_Click" />
+        <asp:Button ID="btnHistorik" runat="server" Text="Se historik" OnClick="btnHistorik_Click" />
+        <asp:Label ID="lblDatum" runat="server" Text="Label"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ JE-Bank/inloggad.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JE_Bank
+{
+    public partial class inloggad : System.Web.UI.Page
+    {
+        Users användare = new Users();
+        Postgres pg = new Postgres();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            användare.Användarnamn = Server.UrlDecode(Request.QueryString["Parameter"].ToString());
+            lblDatum.Text ="Senast godkända test: " + pg.hämtaDatumGodkänd(användare.Användarnamn).ToShortDateString();
+            
+        }
+
+        protected void btnStartaTest_Click(object sender, EventArgs e)
+        {
+             användare.Godkänd = pg.hämtaDatumGodkänd(användare.Användarnamn);
+             
+             Response.Redirect("~/index.aspx?Parameter=" + Server.UrlEncode(användare.Användarnamn));
+ 
+             //if ((användare.Godkänd - DateTime.Now).TotalDays > 365)
+             //{
+             //    Response.Redirect("~/index.aspx?Parameter=" + Server.UrlEncode(användare.Användarnamn));
+             //}
+ 
+             //else
+             //{
+             //    ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('Du har redan gjort ditt prov i år');", true);
+             //}
+        }
+
+        protected void btnHistorik_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
